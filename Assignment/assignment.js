0b37f2980d4a3e2030d6f1ff60a144e00bfb84aa
Ryan Reza <105849761+ryanreza07@users.noreply.github.com>
2023-10-25 17:33:19 +0600

    Add files via upload

---
+++ Assignment/assignment.js
@@ -0,0 +1,14 @@
+//Problem 1: Write a function to calculate the area of a triangle.
+function triangleArea(triangleBase,triangleHeight,){
+    console.log(" Area of a Triangle " + triangleBase*triangleHeight/2);
+}
+//triangleArea(10,20);
+
+
+//Problem 2: Write a function to convert degrees to radians.
+function radians(degree){
+    console.log(degree*3.1415/180);
+}
+//radians(180);
+
+
